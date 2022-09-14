c7c90a4972053ec7454d10e440eda2083d3325f9
TheTrueZK <thetruezk@gmail.com>
2022-09-14 12:16:41 -0500

    started logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 var questionContainer = document.getElementById("question-container");
+var questionElement = document.getElementById("question");
+var answerElements = document.getElementById("answers");
 var startButton = document.getElementById("start-btn");
 var nextButton = document.getElementById("next-btn");
 var highscoresButton = document.getElementById("highscores-btn");
-var shuffledQuestions, currentQuestionIndex
+let shuffledQuestions, currentQuestionIndex
 
 
 startButton.addEventListener('click', startGame)
@@ -16,13 +18,30 @@ function startGame() {
 }
 
 function nextQuestion() {
+    resetState()
     showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
 function showQuestion(question) {
-    
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement("button")
+        button.innerText = answer.text
+        button.classList.add("btn")
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
 }
 
+function resetState() {
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+}
 function selectAnswer() {
     
 }
